68c6f0aee50bb64929cabe7c4c614e2b167ce277
Sri Sai Poosarla <sairampoosarlahyd@gmail.com>
2025-04-21 12:18:54 +1000

    code for password hashing has been moved to user model pre-code from the controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,17 +5,19 @@ const bcrypt = require('bcryptjs')
 //creating a user based on data passed on the request body
 const register = async (req, res) => {
 
+    // password hashing has been moved to pre code of model
     //taking the parameters passed
-    const {name, email, password} = req.body
+    //const {name, email, password} = req.body
 
     //creating the salt and uring it to created password
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(password, salt)
+    //const salt = await bcrypt.genSalt(10)
+    //const hashPassword = await bcrypt.hash(password, salt)
 
     //saving the data into a dict
-    const tempUser = {name, email, password:hashPassword}
+    //const tempUser = {name, email, password:hashPassword}
+    
     //creating the user
-    const user = await User.create({...tempUser})
+    const user = await User.create({...req.body})
     ///returning the creareted user
     res.status(StatusCodes.CREATED).json({user})
 }
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
+//this is the schema we are defining for each object to have before they can be saved in the DB
 const UserSchema = new mongoose.Schema({
     name:{
         type: String,
@@ -10,7 +12,9 @@ const UserSchema = new mongoose.Schema({
     email:{
         type: String,
         required: [true, 'Please provide your email'],
+        //this is the regex for checking a valid email address
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'],
+        //we are saing the email address needs to be unique for a user
         unique: true
     },
     password:{
@@ -19,6 +23,15 @@ const UserSchema = new mongoose.Schema({
         minlenght: 6,
     }
 
+})
+
+// this is a pre-code 
+// this pre-code will run before the document is saved in the database
+// here insted of running the hash in controller we are utlizing the pre-code and hashing the password
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 module.exports = mongoose.model('User', UserSchema)
