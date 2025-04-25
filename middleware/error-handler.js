c1ee23a314cebfca89c8c47d285d461ed4877ef0
Sri Sai Poosarla <sairampoosarlahyd@gmail.com>
2025-04-26 00:06:29 +1000

    custom error code and message has been setup for the status code where duplicates are found

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,19 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
+  //console.log(err)
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong please try again later" 
+  }
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  if (err.code && err.code === 11000){
+    customError.statusCode=400
+    customError.msg = `Duplicate value entered please select another value`
+  }
+  return res.status(customError.statusCode).json({msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
