4bb8652ed8e14a04edc60cb6f355219dadf082d9
Sri Sai Poosarla <sairampoosarlahyd@gmail.com>
2025-04-25 23:54:43 +1000

    added code for updating and deleting the jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,6 @@
 const Job = require("../models/Job")
 const {StatusCodes} = require('http-status-codes')
-const {NotFoundError} = require("../errors")
+const {NotFoundError, BadRequestError} = require("../errors")
 
 //controller to get all the jobs in the database for the requested user
 const getAllJobs = async (req, res) => {
@@ -19,7 +19,21 @@ const getJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-    res.send("Update Job")
+    const user_Id = req.user.userId
+    const object_Id = req.params.id
+    const new_company = req.body.company
+    const new_position = req.body.position
+
+    if(!new_company || !new_position){
+        throw new BadRequestError("Company or Position feild can't be empty")
+    }
+
+    const new_job = await Job.findByIdAndUpdate({_id:object_Id,createdBy:user_Id},{company:new_company,position:new_position}, {new:true,runValidators:true})
+    if(!new_job){
+        throw new NotFoundError(`Requested Object with id ${object_Id} can't be found`)
+    }
+    //console.log(new_job)
+    res.status(StatusCodes.OK).send(new_job)
 }
 
 const createJob = async (req, res) => {
@@ -31,7 +45,16 @@ const createJob = async (req, res) => {
     //res.send("Create Job")
 }
 const deleteJob = async (req, res) => {
-    res.send("Delete Job")
+    const user_Id = req.user.userId
+    const object_Id = req.params.id
+
+    const job_delete = await Job.findByIdAndRemove({_id:object_Id,createdBy:user_Id})
+
+    if (!job_delete) {
+        throw new NotFoundError(`No job with id ${object_Id}`)
+      }
+    res.status(StatusCodes.OK).send()
+
 }
 
 module.exports = {
